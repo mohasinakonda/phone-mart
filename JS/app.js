754c739817c0c0805534f9452f14487e9c2a0812
mohasin akonda <mohasinakonda39@gmail.com>
2022-03-01 17:28:10 +0600

    details feature added

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,7 @@
 const inputValue = document.getElementById("search-value")
 const productsContainer = document.getElementById("products-container")
 
-const ProductsDetailsModal = document.getElementById("modal")
+const ProductsDetailsModal = document.getElementById("products-details")
 document.getElementById("search-btn").addEventListener("click", () => {
 	const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue.value}`
 	fetch(url)
@@ -11,7 +11,9 @@ document.getElementById("search-btn").addEventListener("click", () => {
 	productsContainer.innerHTML = ""
 })
 const getPhones = (phones) => {
-	phones.forEach((phone) => {
+	const twentyPhones = phones.slice(0, 20)
+	twentyPhones.forEach((phone) => {
+		// console.log(phone)
 		const div = document.createElement("div")
 		div.classList.add("card", "col-md-6", "m-2")
 		div.style.width = "18rem"
@@ -34,19 +36,40 @@ const getPhoneDetails = (id) => {
 	fetch(url)
 		.then((res) => res.json())
 		.then((phonesDetails) => getPhonesById(phonesDetails.data))
-	ProductsDetailsModal.classList.remove("d-none")
+	ProductsDetailsModal.textContent = ""
 }
 
+const list = document.getElementById("products-div")
 const getPhonesById = (phone) => {
+	ProductsDetailsModal.innerHTML = ""
+
+	const mainFeatures = phone.mainFeatures
+	const releaseDate = phone.releaseDate
+		? phone.releaseDate
+		: "Release date not available"
+	ProductsDetailsModal.innerHTML = `
+	<div class=" col-6 ">
+					<img class="img-fluid" src="${phone.image}" class="card-img-top" alt="..." />
+					<div class="card-body">
+						<h5 class="card-title">${phone.name}</h5>
+						<p class="card-text">
+						${releaseDate}
+						</p>
+					
+						
+						
+					</div>
+	`
+	features(mainFeatures)
+}
+const features = (mainFeatures) => {
 	const div = document.createElement("div")
-	div.classList.add()
-	div.innerHTML = `
-        <img class="img-fluid" src="${phone.image}" class="card-img-top" alt="..." />
-						<div class="card-body">
-							<h5 class="card-title">${phone.phone_name}</h5>
-							<p class="card-text">${phone.brand}</p>
-							
-						</div>
-        `
-	ProductsDetailsModal.appendChild(div)
+	div.classList.add("col-md-6")
+	for (const [key, value] of Object.entries(mainFeatures)) {
+		const p = document.createElement("p")
+		p.innerHTML = `<span class="fw-bold">${key}</span> &rarr; ${value}`
+
+		div.appendChild(p)
+		ProductsDetailsModal.appendChild(div)
+	}
 }
