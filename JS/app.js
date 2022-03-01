7f71cae10473cbdd35ef632f5dfc1e82966ae9f1
mohasin akonda <mohasinakonda39@gmail.com>
2022-03-01 20:26:37 +0600

    commenting

--- JS/app.js
+++ JS/app.js
@@ -19,24 +19,30 @@ document.getElementById("search-btn").addEventListener("click", () => {
 	productsContainer.innerHTML = ""
 })
 const getPhones = (phones) => {
+	document.getElementById("showAll-btn").classList.remove("d-none")
+
 	const twentyPhones = phones.slice(0, 20)
 	twentyPhones.forEach((phone) => {
 		// console.log(phone)
+
 		const div = document.createElement("div")
+
 		div.classList.add("card", "col-md-6", "m-2")
 		div.style.width = "18rem"
 		div.innerHTML = `
-        
-						<img src="${phone.image}" class="card-img-top" alt="..." />
-						<div class="card-body">
-							<h5 class="card-title">${phone.phone_name}</h5>
-							<p class="card-text">${phone.brand}</p>
+			
+							<img src="${phone.image}" class="card-img-top" alt="..." />
+							<div class="card-body">
+								<h5 class="card-title">${phone.phone_name}</h5>
+								<p class="card-text">${phone.brand}</p>
+								
+							</div>
 							<button onclick=getPhoneDetails('${phone.slug}')  class="btn btn-outline-info ">show details</button>
-						</div>
-					
-        `
+						
+			`
 		productsContainer.appendChild(div)
 	})
+
 	ProductsDetailsModal.textContent = ""
 }
 
@@ -60,7 +66,7 @@ const getPhonesById = (phone) => {
 		? phone.releaseDate
 		: "Release date not available"
 	ProductsDetailsModal.innerHTML = `
-	<div class=" col ">
+	<div class=" col-12 ">
 					<img class="img-fluid" src="${phone.image}" class="card-img-top" alt="..." />
 					<div class="card-body">
 						<h5 class="card-title">${phone.name}</h5>
@@ -72,15 +78,15 @@ const getPhonesById = (phone) => {
 						
 					</div>
 	`
-	features(mainFeatures)
+	features(mainFeatures, "Main feature :")
 	sensorsList(sensors)
-	otherFeatures(otherFeature)
+	features(otherFeature, "Other feature :")
 }
-const features = (mainFeatures) => {
+const features = (mainFeatures, title) => {
 	const div = document.createElement("div")
 	// main features
 	const h3 = document.createElement("h3")
-	h3.innerHTML = "Main features :"
+	h3.innerHTML = title
 	div.appendChild(h3)
 	div.classList.add("col-12")
 	for (const [key, value] of Object.entries(mainFeatures)) {
@@ -90,23 +96,6 @@ const features = (mainFeatures) => {
 		p.innerHTML = `<span class="fw-bold">${key}</span> &rarr; ${value}`
 		div.appendChild(p)
 
-		ProductsDetailsModal.appendChild(div)
-	}
-}
-
-const otherFeatures = (others) => {
-	const div = document.createElement("div")
-
-	// other features
-	const h3 = document.createElement("h3")
-	h3.innerHTML = "Others features :"
-	div.appendChild(h3)
-	div.classList.add("col-12")
-	for (const [key, value] of Object.entries(others)) {
-		const p = document.createElement("p")
-		p.innerHTML = `<span class="fw-bold">${key}</span> &rarr; ${value}`
-
-		div.appendChild(p)
 		ProductsDetailsModal.appendChild(div)
 	}
 }
